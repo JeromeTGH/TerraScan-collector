abb888c01d579ccb59744aeb8548d4593fb52f1d
JeromeTGH <jerome.tomski@gmx.fr>
2023-10-01 20:58:59 +0200

    Ajout des calculs déterminants les unités de temps (H1, H4, D1, W1, M1, et Y1)

--- utils/dbwriter/dbwriter.go
+++ utils/dbwriter/dbwriter.go
@@ -17,6 +17,29 @@ import (
 
 func WriteTotalSuppliesInDb(dataFromLcd lcd.StructReponseTotalSupplies) {
 
+	// Génération des valeurs à enregistrer
+	currentTime := time.Now().UTC()
+
+	nAnnee := currentTime.Year()
+	nMois := currentTime.Month()
+	nJours := currentTime.Day()
+	nHeures := currentTime.Hour()
+	nMinutes := currentTime.Minute()
+	nSecondes := currentTime.Second()
+	dayOfTheWeek := currentTime.Weekday()		// Dimanche = 0
+
+	code := fmt.Sprintf("%d%02d%02d%02d%02d", nAnnee, nMois, nJours, nHeures, nMinutes)
+	dateUTCpourMysql := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02dZ", nAnnee, nMois, nJours, nHeures, nMinutes, nSecondes)
+
+	bH1 := true;
+	bH4 := (nHeures == 0 || nHeures == 4 || nHeures == 8 || nHeures == 12 || nHeures == 16 || nHeures == 20);
+	bD1 := nHeures == 0;
+	bW1 := (dayOfTheWeek == 1 && nHeures == 0);       // Lundi à Oh...
+	bM1 := (nJours == 1 && nHeures == 0);
+	bY1 := (nMois == 1 && nJours == 1 && nHeures == 0);
+
+
+
 	// Afichage dans la console de ces données (debug)
 	fmt.Printf("LUNCtotalSupply = %d\n", dataFromLcd.LuncTotalSupply)
 	fmt.Printf("USTCtotalSupply = %d\n", dataFromLcd.UstcTotalSupply)
@@ -36,13 +59,13 @@ func WriteTotalSuppliesInDb(dataFromLcd lcd.StructReponseTotalSupplies) {
 	db.SetMaxIdleConns(10)
 
 
-	// Effacement de la table
-	rqtZero := "DROP TABLE IF EXISTS tblTotalSupplies2"
-	_, errZero := db.ExecContext(context.Background(), rqtZero)
-	if errZero != nil {  
-		fmt.Printf("Error %s when dropping table", errZero)
-		return
-	}
+	// // Effacement de la table
+	// rqtZero := "DROP TABLE IF EXISTS tblTotalSupplies2"
+	// _, errZero := db.ExecContext(context.Background(), rqtZero)
+	// if errZero != nil {  
+	// 	fmt.Printf("Error %s when dropping table", errZero)
+	// 	return
+	// }
 
 	// Construction de la requête
 	rqt := "INSERT INTO tblTotalSupplies2 VALUES ("
@@ -61,7 +84,7 @@ func WriteTotalSuppliesInDb(dataFromLcd lcd.StructReponseTotalSupplies) {
 
 	// Exécution de la requête
 	var bCreateTableNeeded = false
-	insertResult, err2 := db.ExecContext(context.Background(), rqt, "code", "datetimeUTC", true, false, false, false, false, false, dataFromLcd.LuncTotalSupply, dataFromLcd.UstcTotalSupply)
+	insertResult, err2 := db.ExecContext(context.Background(), rqt, code, dateUTCpourMysql, bH1, bH4, bD1, bW1, bM1, bY1, dataFromLcd.LuncTotalSupply, dataFromLcd.UstcTotalSupply)
 	if err2 != nil {
 		if strings.Contains(err2.Error(), "doesn't exist") {
 			bCreateTableNeeded = true
@@ -95,7 +118,7 @@ func WriteTotalSuppliesInDb(dataFromLcd lcd.StructReponseTotalSupplies) {
 			return
 		}
 
-		insertResult2, err2c := db.ExecContext(context.Background(), rqt, "code", "datetimeUTC", true, false, false, false, false, false, dataFromLcd.LuncTotalSupply, dataFromLcd.UstcTotalSupply)
+		insertResult2, err2c := db.ExecContext(context.Background(), rqt, code, dateUTCpourMysql, bH1, bH4, bD1, bW1, bM1, bY1, dataFromLcd.LuncTotalSupply, dataFromLcd.UstcTotalSupply)
 		if err2c != nil {
 			fmt.Printf("insert failed : %s", err2c)
 			return
