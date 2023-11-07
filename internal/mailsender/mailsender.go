31d8751dfbba4b8c234fe4da3a21944728e9f707
JeromeTGH <jerome.tomski@gmx.fr>
2023-11-07 10:29:32 +0100

    Ajout message d'erreur détaillé, en cas de failed du mailsender

--- internal/mailsender/mailsender.go
+++ internal/mailsender/mailsender.go
@@ -17,7 +17,7 @@ func Sendmail(sujet string, body string, channelForLogsMsgs chan<- string) {
 	dialer := gomail.NewDialer(config.AppConfig.Email.HostName, config.AppConfig.Email.SmtpPort, config.AppConfig.Email.From, config.AppConfig.Email.Pwd)
 
 	if err := dialer.DialAndSend(message); err != nil {
-		channelForLogsMsgs <- "[mailsender] failed to send mail"
+		channelForLogsMsgs <- "[mailsender] failed to send mail" + err.Error()
 	} else {
 		channelForLogsMsgs <- "[mailsender] email sent successfully"
 	}
